51f27108cc5f747643233138134f76d41c38510a
vanamraghu <raghavendra.gv.vanam@gmail.com>
2016-09-25 14:38:20 +0530

    Test Case about the methods that will be tested

---
+++ TestCase01.java
@@ -0,0 +1,26 @@
+package testCases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import pages.LogIn;
+
+public class TestCase01 {
+	
+
+	WebDriver driver = new FirefoxDriver();
+	LogIn data = new LogIn(driver);
+	
+  @Test(priority=0)
+  public void openingSite() {
+	  data.webSite("https://wordpress.com/wp-login.php");
+	  
+	  
+  }
+  @Test(dependsOnMethods="openingSite")
+  public void logInTest()
+  {
+	  data.logIn("dddf", "ssdfd");
+  }
+  
